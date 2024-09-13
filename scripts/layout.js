c00ba03cdd53718e835c6febc2f4ed0fab10d4a8
tinajoelly <tinaandrea.joelly2@edu.fh-kaernten.ac.at>
2024-09-13 21:42:58 +0200

    finish card layout

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,11 +1,16 @@
-function getPokemonCardTemplate(pokemon) {
-  return `<div class="poke-container bg-${pokemon.types[0].type.name}">
+function getPokeCardTypesTemplate(pokeType) {
+  return `<div class="poke-type-icon bg-${pokeType}">
+  <img class="type-icon" src="../assets/icons/${pokeType}.svg"
+  </div>`;
+}
+
+function getPokemonCardTemplate(index, pokemon) {
+  return `<div class="poke-container bg-${pokemon.types[0].type.name}" onclick="showPokeDetailDialog(event, ${index})">
           <div class="poke-card-bg">#${pokemon.id}</div>
             <img class="poke-img" src="${pokemon.sprites.front_default}" />
           <div class="poke-card-info">
             <h2>${pokemon.name}</h2>
-            <div class="poke-type-row">
-              <div class="poke-type-icon"></div>
+            <div id="poke-type-${index}" class="poke-type-row">
             </div>
           </div>
         </div>`;
@@ -15,6 +20,20 @@ function renderPokemonCards() {
   let card = document.getElementById("poke-cards");
   for (let i = 0; i < pokeArray.length; i++) {
     const pokemon = pokeArray[i];
-    card.innerHTML += getPokemonCardTemplate(pokemon);
+    card.innerHTML += getPokemonCardTemplate(i, pokemon);
+    getPokeCardTypes(i, pokemon.types);
   }
+}
+
+function getPokeCardTypes(pokeIndex, types) {
+  let typeRow = document.getElementById(`poke-type-${pokeIndex}`);
+  for (let index = 0; index < types.length; index++) {
+    const pokeType = types[index];
+    typeRow.innerHTML += getPokeCardTypesTemplate(pokeType.type.name);
+  }
+}
+
+function clearPokeCards() {
+  let card = document.getElementById("poke-cards");
+  card.innerHTML = "";
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
+let pokeCount = 20;
+
 async function init() {
-  pokeArray = await getAllPokemons(20);
-  console.log(pokeArray);
+  pokeArray = await getAllPokemons(pokeCount);
   renderPokemonCards();
+}
+
+async function loadMorePokemons() {
+  pokeCount += 20;
+  pokeArray = [];
+  clearPokeCards();
+  pokeArray = await getAllPokemons(pokeCount);
+  renderPokemonCards();
+}
+
+function closePokeDetailDialog(event) {
+  let overlay = document.getElementById("overlay-window");
+  overlay.classList.add("hidden");
+  overlay.innerHTML = "";
+  event.stopPropagation();
+}
+
+function showPokeDetailDialog(event, index) {
+  let overlay = document.getElementById("overlay-window");
+  overlay.classList.remove("hidden");
+  event.stopPropagation();
 }
